00c1684c2d277169e68ca61fe4e82a71c803183f
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-01-05 14:24:17 +0530

    Add a factory contract to deploy and track wallets

--- contracts/src/Factory.sol
+++ contracts/src/Factory.sol
@@ -2,6 +2,27 @@
 
 pragma solidity ^0.8.20;
 
+import {MultiSigWallet} from "./MultiSigWallet.sol";
+
 contract Factory {
-    constructor() {}
+    mapping(address => address) private ownersAndWallets;
+
+    function deployWallet(
+        address[] memory _owners,
+        uint256 _requiredApprovals
+    ) public {
+        MultiSigWallet multiSigWalletwallet = new MultiSigWallet(
+            _owners,
+            _requiredApprovals
+        );
+        for (uint256 count = 0; count < _owners.length; count++) {
+            ownersAndWallets[_owners[count]] = address(multiSigWalletwallet);
+        }
+    }
+
+    function getOwnersWalletAddress(
+        address _owner
+    ) public view returns (address) {
+        return ownersAndWallets[_owner];
+    }
 }
