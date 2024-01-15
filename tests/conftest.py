70a6ee1c4173a817108bd7b2b63658ab75eab814
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-01-15 13:43:59 +0530

    tets: Add more useful fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,3 +21,26 @@ def not_owner(accounts):
 def wallet(owners, project):
     constructor_args = [owners, 2]
     return project.MultiSigWallet.deploy(*constructor_args, sender=owners[0])
+
+
+@pytest.fixture(scope="session")
+def token_contract(owners, project, web3):
+    constructor_args = [web3.to_wei(10, "ether")]
+    return project.TestToken.deploy(*constructor_args, sender=owners[0])
+
+
+@pytest.fixture(scope="session")
+def issue_eth_transfer_transaction_request(owners, not_owner, wallet):
+    return lambda account: wallet.issueETHTransferTransactionRequest(
+        *[not_owner, "1 ether"], sender=account
+    )
+
+
+@pytest.fixture(scope="session")
+def issue_token_transfer_transaction_request(
+    not_owner, owners, wallet, token_contract, web3
+):
+    args = [token_contract, 0, not_owner, web3.to_wei(0.5, "ether")]
+    return lambda account: wallet.issueTokenTransferTransactionRequest(
+        *args, sender=account
+    )
