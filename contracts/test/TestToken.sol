dff49b14b73557d4e24c3af4c82d6f1c6924e16d
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-01-16 13:40:31 +0530

    fix: Change the import path

--- contracts/test/TestToken.sol
+++ contracts/test/TestToken.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("TestToken", "TT") {
